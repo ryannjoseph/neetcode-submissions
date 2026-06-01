98f6979aed51d3ad7501c3d5666819b1125b3471
ryannjoseph <146375885+ryannjoseph@users.noreply.github.com>
2026-05-31 20:56:11 -0500

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,15 @@
+# Input: Array named nums, int named targt 
+# Output:Returning Indices i and k to equal a targt
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indices = {}  # val -> index
+
+        for i, n in enumerate(nums):
+            indices[n] = i
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in indices and indices[diff] != i:
+                return [i, indices[diff]]
+        return []
